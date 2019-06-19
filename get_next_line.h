ee1b4601fe0e279daa0c679cd316fec1a67a69bf
Bradleigh MARKS <bmarks@c3r1s7.wethinkcode.co.za>
2019-06-19 11:41:44 +0200

    gnl done v2

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmarks <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 09:38:46 by bmarks            #+#    #+#             */
+/*   Updated: 2019/06/14 16:38:25 by bmarks           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 100
+
+# include "libft/includes/libft.h"
+
+int	get_next_line(const int fd, char **line);
+
+#endif
